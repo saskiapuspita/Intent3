a00a1531412b04cf6f6586eaeb9fa21a57b7798c
SaskiaPuspita <dewsaski@gmail.com>
2016-10-09 09:33:10 +0700

    Intent Camera (Photo).

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -1,19 +1,24 @@
 package id.sch.smktelkom_mlg.learn.intent3;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.imagePhone).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.imageViewPhone).setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -21,19 +26,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.imageSMS).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.imageViewSMS).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 composeSmsMessage("Pesan dari SMK Telkom Malang");
             }
         });
 
-        findViewById(R.id.imageBrowser).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.imageViewBrowser).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openWebPage("http://www.smktelkom-mlg.sch.id/");
             }
         });
+
+        findViewById(R.id.imageViewCamera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                capturePhoto();
+            }
+        });
+    }
+
+    private void capturePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bitmap bitmap = data.getParcelableExtra("data");
+            ImageView iv = (ImageView) findViewById(R.id.imageViewCamera);
+            iv.setImageBitmap(bitmap);
+        }
     }
 
     private void openWebPage(String url) {
